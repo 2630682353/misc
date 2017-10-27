ad83f84cb5dceb0730c9cbb19a59d7d30bd9ad95
zhoucong <2630682353@qq.com>
2017-10-27 18:13:14 +0800

    add

---
+++ netlink_kernel_client.c
@@ -0,0 +1,143 @@
+/****************************************
+* Author: zhangwj
+* Date: 2017-01-19
+* Filename: netlink_test.c
+* Descript: netlink of kernel
+* Kernel: 3.10.0-327.22.2.el7.x86_64
+* Warning:
+******************************************/
+
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/types.h>
+#include <net/sock.h>
+#include <linux/netlink.h>
+
+#define NETLINK_TEST     30
+#define MSG_LEN            125
+#define USER_PORT        100
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("zhangwj");
+MODULE_DESCRIPTION("netlink example");
+
+enum {
+	NLK_DPI = 0,
+	NLK_WEBSERVER,
+	NLK_GATEWAY
+};
+
+struct sock *nlsk = NULL;
+extern struct net init_net;
+static int i;
+static struct timer_list zc_timer;
+static int send_to_user(int index);
+
+int grp = 1;
+module_param(grp,int,S_IRUSR);
+
+
+/*
+static void netlink_rcv_msg(struct sk_buff *skb)
+{
+    struct nlmsghdr *nlh = NULL;
+    char *umsg = NULL;
+    char *kmsg = "hello users!!!";
+
+    if(skb->len >= nlmsg_total_size(0))
+    {
+        nlh = nlmsg_hdr(skb);
+        umsg = NLMSG_DATA(nlh);
+        if(umsg)
+        {
+            printk("kernel recv from user: %s\n", umsg);
+            send_usrmsg(kmsg, strlen(kmsg));
+        }
+    }
+}
+*/
+
+struct netlink_kernel_cfg cfg = { 
+//        .input  = netlink_rcv_msg, /* set recv callback */
+		.groups = 1 << NLK_DPI
+};  
+
+static void timer_do_func(unsigned long arg)
+{
+	zc_timer.expires = jiffies + 10 * HZ;
+	i++;
+	printk(KERN_DEBUG "now is %d ... %d\n", i, grp);
+	send_to_user(i);
+	add_timer(&zc_timer);
+	return;
+}
+
+
+
+
+static int send_to_user(int index)
+{
+	struct sk_buff *nl_skb;
+    struct nlmsghdr *nlh;
+	char msg[20] = "Hello from kernel";
+    int ret;
+
+    /* 创建sk_buff 空间 */
+    nl_skb = nlmsg_new(MSG_LEN, GFP_ATOMIC);
+    if(!nl_skb)
+    {
+        printk("netlink alloc failure\n");
+        return -1;
+    }
+
+    /* 设置netlink消息头部 */
+    nlh = nlmsg_put(nl_skb, 0, 0, NLMSG_DONE, MSG_LEN, 0);
+    if(nlh == NULL)
+    {
+        printk("nlmsg_put failaure \n");
+        nlmsg_free(nl_skb);
+        return -1;
+    }
+
+    /* 拷贝数据发送 */
+    memcpy(nlmsg_data(nlh), msg, sizeof(msg));
+	memset(nlmsg_data(nlh)+strlen(msg), grp+48, 1);
+	
+    ret = nlmsg_multicast(nlsk, nl_skb, 0, grp, MSG_DONTWAIT);
+	if (ret < 0) 
+		printk(KERN_INFO "Error while sending bak to user, err id: %d\n", ret);
+	return 0;
+}
+
+int test_netlink_init(void)
+{
+    /* create netlink socket */
+    nlsk = (struct sock *)netlink_kernel_create(&init_net, NETLINK_TEST, &cfg);
+    if(nlsk == NULL)
+    {   
+        printk("netlink_kernel_create error !\n");
+        return -1; 
+    }   
+    printk("test_netlink_init\n");
+	init_timer(&zc_timer);
+	zc_timer.function = timer_do_func;
+	zc_timer.expires = jiffies + 10 * HZ;
+	add_timer(&zc_timer);
+	printk(KERN_DEBUG "timer add ...\n");
+	i = 0;
+    return 0;
+}
+
+void test_netlink_exit(void)
+{
+	del_timer(&zc_timer);
+    if (nlsk){
+        netlink_kernel_release(nlsk); /* release ..*/
+        nlsk = NULL;
+    }   
+    printk("test_netlink_exit!\n");
+}
+
+module_init(test_netlink_init);
+module_exit(test_netlink_exit);
+
---
+++ netlink_user_server.c
@@ -0,0 +1,142 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <string.h>
+#include <linux/netlink.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <errno.h>
+
+#define NETLINK_TEST    30
+#define MSG_LEN            125
+#define MAX_PLOAD        125
+
+enum {
+	NLK_DPI = 0,
+	NLK_WEBSERVER,
+	NLK_GATEWAY
+};
+
+typedef struct _user_msg_info
+{
+    struct nlmsghdr hdr;
+    char  msg[MSG_LEN];
+} user_msg_info;
+
+int netlink_init()
+{
+	int skfd;
+	socklen_t len;
+    struct nlmsghdr *nlh = NULL;
+    struct sockaddr_nl saddr, daddr;
+	skfd = socket(AF_NETLINK, SOCK_DGRAM, NETLINK_TEST);
+	if(skfd < 0)
+    {
+        perror("create socket error\n");
+        return -1;
+    }
+	int grp = 0;
+	grp |= 1 << NLK_DPI;
+	grp |= 1 << NLK_WEBSERVER;
+	grp |= 1 << NLK_GATEWAY;
+	grp |= 1 << 3;
+	grp |= 1 << 4;
+	memset(&saddr, 0, sizeof(saddr));
+    saddr.nl_family = AF_NETLINK; //AF_NETLINK
+    saddr.nl_pid = getpid();
+    saddr.nl_groups = grp;
+	if(bind(skfd, (struct sockaddr *)&saddr, sizeof(saddr)) < 0)
+    {
+        perror("bind() error\n");
+        close(skfd);
+        return -1;
+    }
+	return skfd;
+	
+}
+
+/*
+int main(int argc, char **argv)
+{
+    int skfd;
+    int ret;
+    user_msg_info u_info;
+    socklen_t len;
+    struct nlmsghdr *nlh = NULL;
+    struct sockaddr_nl saddr, daddr;
+    char *umsg = "hello netlink!!";
+
+    skfd = socket(AF_NETLINK, SOCK_DGRAM, NETLINK_TEST);
+    if(skfd == -1)
+    {
+        perror("create socket error\n");
+        return -1;
+    }
+
+    memset(&saddr, 0, sizeof(saddr));
+    saddr.nl_family = AF_NETLINK; //AF_NETLINK
+    saddr.nl_pid = 100;  //端口号(port ID) 
+    saddr.nl_groups = 0;
+    if(bind(skfd, (struct sockaddr *)&saddr, sizeof(saddr)) != 0)
+    {
+        perror("bind() error\n");
+        close(skfd);
+        return -1;
+    }
+
+    memset(&daddr, 0, sizeof(daddr));
+    daddr.nl_family = AF_NETLINK;
+    daddr.nl_pid = 0; // to kernel 
+    daddr.nl_groups = 0;
+
+    nlh = (struct nlmsghdr *)malloc(NLMSG_SPACE(MAX_PLOAD));
+    memset(nlh, 0, sizeof(struct nlmsghdr));
+    nlh->nlmsg_len = NLMSG_SPACE(MAX_PLOAD);
+    nlh->nlmsg_flags = 0;
+    nlh->nlmsg_type = 0;
+    nlh->nlmsg_seq = 0;
+    nlh->nlmsg_pid = saddr.nl_pid; //self port
+
+    memcpy(NLMSG_DATA(nlh), umsg, strlen(umsg));
+    ret = sendto(skfd, nlh, nlh->nlmsg_len, 0, (struct sockaddr *)&daddr, sizeof(struct sockaddr_nl));
+    if(!ret)
+    {
+        perror("sendto error\n");
+        close(skfd);
+        exit(-1);
+    }
+    printf("send kernel:%s\n", umsg);
+
+    memset(&u_info, 0, sizeof(u_info));
+    len = sizeof(struct sockaddr_nl);
+    ret = recvfrom(skfd, &u_info, sizeof(user_msg_info), 0, (struct sockaddr *)&daddr, &len);
+    if(!ret)
+    {
+        perror("recv form kernel error\n");
+        close(skfd);
+        exit(-1);
+    }
+
+    printf("from kernel:%s\n", u_info.msg);
+    close(skfd);
+
+    free((void *)nlh);
+    return 0;
+}
+*/
+
+int main()
+{
+	int fd = netlink_init();
+	user_msg_info u_info;
+	int len = 0;
+	while(1){
+		memset(&u_info, 0, sizeof(u_info));
+	
+		len = recvfrom(fd, &u_info, sizeof(user_msg_info), 0, NULL, NULL);
+		printf("size: %d  %s\n", len, u_info.msg);
+
+	}
+
+}
+
